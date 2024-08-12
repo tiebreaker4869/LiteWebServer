9fd847db466665ecdc503f554ae33c60ea278cdc
Lifan Sun <lifansun1412@gmail.com>
2024-08-12 10:47:43 +0800

    SQL 连接池实现，TODO: 缺少日志输出

---
+++ src/sql/sql_connection_pool.cpp
@@ -0,0 +1,120 @@
+#include "sql_connection_pool.h"
+
+SQLConnPool::SQLConnPool() {
+    cur_connection_ = 0;
+    free_connection_ = 0;
+}
+
+SQLConnPool* SQLConnPool::GetInstance() {
+    static SQLConnPool pool;
+    return &pool;
+}
+
+void SQLConnPool::Init(SQLConfig config) {
+    this->url = config.url;
+    this->db_name = config.db_name;
+    this->username = config.username;
+    this->password = config.password;
+    this->port = config.port;
+    this->close_log = config.close_log;
+    this->max_connection_ = config.max_connection;
+
+    for (int i = 0; i < max_connection_; i ++) {
+        MYSQL* conn = mysql_init(nullptr);
+
+        if (!conn) {
+            // log error info
+            exit(1);
+        }
+
+        conn = mysql_real_connect(conn, url.c_str(), username.c_str(), password.c_str(), db_name.c_str(), port, nullptr, 0);
+
+        if (!conn) {
+            // log error info
+            exit(1);
+        }
+
+        pool_.push_back(conn);
+    }
+
+    free_connection_ = max_connection_;
+    reserve_ = Semaphore(free_connection_);
+}
+
+// 从数据库连接池中拿出一个空闲连接
+MYSQL* SQLConnPool::GetConnection() {
+    MYSQL* conn = nullptr;
+
+    if (pool_.size() == 0) {
+        return nullptr;
+    }
+
+    // 等待空闲连接
+    reserve_.Wait();
+
+    // 独占访问连接池
+    mutex_.Lock();
+
+    conn = pool_.front();
+    pool_.pop_front();
+
+    -- free_connection_;
+    ++ cur_connection_;
+
+    mutex_.Unlock();
+    return conn;
+}
+
+// 归还连接到数据库连接池
+bool SQLConnPool::ReleaseConnection(MYSQL* conn) {
+    if (!conn) {
+        return false;
+    }
+
+    mutex_.Lock();
+
+    pool_.push_back(conn);
+    ++ free_connection_;
+    -- cur_connection_;
+
+    mutex_.Unlock();
+
+    reserve_.Post();
+    return true;
+}
+
+// 释放所有连接
+void SQLConnPool::DestroyPool() {
+    mutex_.Lock();
+
+    if (!pool_.empty()) {
+        for (auto it = pool_.begin(); it != pool_.end(); it ++) {
+            MYSQL* conn = *it;
+            mysql_close(conn);
+        }
+
+        cur_connection_ = 0;
+        free_connection_ = 0;
+        pool_.clear();
+    }
+
+    mutex_.Unlock();
+}
+
+int SQLConnPool::GetNumFreeConn() const {
+    return free_connection_;
+}
+
+SQLConnPool::~SQLConnPool() {
+    DestroyPool();
+}
+
+ConnectionRAIIWrapper::ConnectionRAIIWrapper(MYSQL** conn, SQLConnPool* conn_pool) {
+    *conn = conn_pool->GetConnection();
+    conn_ = *conn;
+    pool_ = conn_pool;
+}
+
+ConnectionRAIIWrapper::~ConnectionRAIIWrapper() {
+    pool_->ReleaseConnection(conn_);
+}
